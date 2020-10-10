1f47c88b4c16edfe70590d870424ec3f7a4f3e46
Andrew Bauer <andenrx@gmail.com>
2020-10-10 16:20:40 -0700

    Separated the api-key from the script. Handle NaNs

--- script.py
+++ script.py
@@ -3,12 +3,21 @@ import requests
 import sys
 import pandas as pd
 
-if len(sys.argv) != 3:
-	raise Exception("need to pass in show name then key")
-
+if len(sys.argv) != 2:
+	raise Exception("Must use as './script.py futurama'")
 SHOW = sys.argv[1]
-KEY  = sys.argv[2]
 
+try:
+	with open("apikey") as file:
+		KEY = file.read().strip()
+except FileNotFoundError:
+	raise FileNotFoundError(f"Missing omdbapi key. Store it in file 'apikey'.")
+
+def handle_nan(n):
+	try:
+		return float(n)
+	except ValueError as e:
+		return float('nan')
 
 def from_api():
 	show_info = requests.get(f"http://www.omdbapi.com/?t={SHOW}&apikey={KEY}").json()
@@ -22,18 +31,16 @@ def from_api():
 			temp = requests.get(f"http://www.omdbapi.com/?i={id}&Season={i}&apikey={KEY}").json()
 			seasons[i] = {}
 			for entry in temp['Episodes']:
-				seasons[i][int(entry['Episode'])] = (entry['Title'], float(entry['imdbRating']))
+				seasons[i][int(entry['Episode'])] = (entry['Title'], handle_nan(entry['imdbRating']))
 		return seasons
 
 	seasons = f(id, total_seasons, KEY)
 
 	temp = []
-	k = 0
 	for i, s in seasons.items():
 		for j, e in s.items():
-			temp.append([k, i, j, *e])
-			k += 1
-	df = pd.DataFrame(temp, columns=["id", "season", "episode", "title", "rating"])
+			temp.append([i, j, *e])
+	df = pd.DataFrame(temp, columns=["season", "episode", "title", "rating"])
 	df.to_csv(f"{SHOW}_data.csv")
 	return df
 
@@ -41,4 +48,5 @@ try:
 	df = pd.read_csv(f"{SHOW}_data.csv")
 except FileNotFoundError:
 	df = from_api()
+
 print(df)
